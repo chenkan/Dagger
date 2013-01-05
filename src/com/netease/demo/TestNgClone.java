714206b6063aa0d285f0b0dec1141183eca39ae5
ChenKan <ckbsc@126.com>
2013-01-05 21:51:38 +0800

    Add demo

---
+++ src/com/netease/demo/TestNgClone.java
@@ -0,0 +1,41 @@
+package com.netease.demo;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import com.netease.dagger.BrowserEmulator;
+
+public class TestNgClone {
+
+	String googleUrl = "http://www.google.com";
+	String searchBox = "//input[@name='q']";
+	String searchBtn = "//input[@name='btnK']";
+	BrowserEmulator be;
+
+	@BeforeClass
+	public void doBeforeClass() throws Exception {
+		be = new BrowserEmulator();
+	}
+
+	@Test(dataProvider = "data")
+	public void doTest(String keyword, String result) {
+		be.open(googleUrl);
+		be.type(searchBox, keyword);
+		be.click(searchBtn);
+		be.expectTextExistOrNot(true, result, 5000);
+	}
+
+	@AfterClass(alwaysRun = true)
+	public void doAfterClass() {
+		be.quit();
+	}
+
+	@DataProvider(name = "data")
+	public Object[][] data() {
+		return new Object[][] { 
+				{ "java", "www.java.com" }, 
+				{ "github", "https://github.com/" }, 
+			};
+	}
+}
